86cb0c61485f24aa0a40fd8636295b56d35576ff
Sean Keery <skibum55@hotmail.com>
2016-01-14 18:44:04 -0700

    decode to utf-8

--- crashed.py
+++ crashed.py
@@ -10,11 +10,11 @@ for org in orgs['resources']:
     name = org['entity']['name']
     guid = org['metadata']['guid']
     spaces_url = org['entity']['spaces_url']
-    spaces = json.loads(check_output(['cf', 'curl', spaces_url]))
+    spaces = json.loads(check_output(['cf', 'curl', spaces_url]).decode('utf8'))
     for space in spaces['resources']:
         apps_url = space['entity']['apps_url']
         consumed = 0
-        apps = json.loads(check_output(['cf', 'curl', apps_url]))
+        apps = json.loads(check_output(['cf', 'curl', apps_url]).decode('utf8'))
         print "Org " + name + " - Space " + space['entity']['name']
         for app in apps['resources']:
             instances = app['entity']['instances']
@@ -22,8 +22,7 @@ for org in orgs['resources']:
             consumed += (instances * memory)
             if app['entity']['state'] <> 'STOPPED':
                 instances_url = app['metadata']['url'] + '/instances'
-                instances = json.loads(check_output(['cf', 'curl', instances_url]))
+                instances = json.loads(check_output(['cf', 'curl', instances_url]).decode('utf8'))
                 for i in instances:
-#                    print  instances[i]['state']
                      state = instances[i]['state']
                      print "\tState is " + state + " for " + app['entity']['name'] + '/' + i
