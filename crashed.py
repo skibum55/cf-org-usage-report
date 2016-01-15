4bc9eb175b1bda2ceecaa33347fdf5880cac4acd
Sean Keery <skibum55@hotmail.com>
2016-01-14 17:42:19 -0700

    crashed only

--- crashed.py
+++ crashed.py
@@ -9,17 +9,13 @@ orgs = json.loads(check_output(['cf', 'curl', '/v2/organizations']))
 for org in orgs['resources']:
     name = org['entity']['name']
     guid = org['metadata']['guid']
-    quota_url = org['entity']['quota_definition_url']
-    quota = json.loads(check_output(['cf', 'curl', quota_url]))
-    quota_memeory_limit = quota['entity']['memory_limit']
-    used = json.loads(check_output(['cf', 'curl', '/v2/organizations/' + guid + '/memory_usage']))['memory_usage_in_mb']
-    print "Org " + name + " is using " + str(used) + "MB of " + str(quota_memeory_limit) + "MB."
     spaces_url = org['entity']['spaces_url']
     spaces = json.loads(check_output(['cf', 'curl', spaces_url]))
     for space in spaces['resources']:
         apps_url = space['entity']['apps_url']
         consumed = 0
         apps = json.loads(check_output(['cf', 'curl', apps_url]))
+        print "Org " + name + " - Space " + space['entity']['name']
         for app in apps['resources']:
             instances = app['entity']['instances']
             memory = app['entity']['memory']
